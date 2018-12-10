d5f7428a6dc9d380be667879e8aa67a264a3e1e1
georgiy <mail@api.haus>
2018-12-10 17:38:12 +0300

    feat: improve ascii art

--- src/ascii-art.js
+++ src/ascii-art.js
@@ -2,16 +2,21 @@ import packageJson from '../package.json';
 
 const year = new Date().getFullYear();
 
-const asciiArt = `       ____			Loading Scraper Node ${packageJson.version}...
-      /___/\\_			
-     _\\   \\/_/\\__						
-   __\\       \\/_/\\					
-   \\   __    __ \\ \\					
-  __\\  \\_\\   \\_\\ \\ \\   __			
- /_/\\\\   __   __  \\ \\_/_/\\			
- \\_\\/_\\__\\/\\__\\/\\__\\/_\\_\\/		
-    \\_\\/_/\\       /_\\_\\/		Copyright Ⓒ ${year} 
-       \\_\\/       \\_\\/		All Rights Reserved
+const asciiArt = `
+=============== Loading Scraper Node ${packageJson.version}... ===============
+
+                                                      
+    .d8888b  .d8888b888d888 8888b. 88888b.  .d88b. 888d888 
+    88K     d88P"   888P"      "88b888 "88bd8P  Y8b888P"   
+    "Y8888b.888     888    .d888888888  88888888888888     
+         X88Y88b.   888    888  888888 d88PY8b.    888     
+    88888P'  "Y8888P888    "Y88888888888P"  "Y8888 888     
+                                   888                     
+                                   888                     
+                                   888     
+								                
+	Copyright Ⓒ ${year}
+	All Rights Reserved
 `;
 
 export default function showAsciiArt() {
