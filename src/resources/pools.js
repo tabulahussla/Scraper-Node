24b790d1da5b514042a3a8d4b98b9fe36824b8e8
georgiy <mail@api.haus>
2019-01-24 17:50:23 +0300

    fix

--- src/resources/pools.js
+++ src/resources/pools.js
@@ -1,18 +1,23 @@
 import config from 'config';
+import log from 'common/log';
 
 const resourceBrokerConfig = { ...config.get('resource-broker') };
 const { pools = {} } = resourceBrokerConfig;
 
 const resourcesInPool = new Map();
 
+/*
+"pools": {
+	"RESOURCE_TYPE": ["POOL_ID_1", "POOL_ID_2"],
+},
+*/
 for (const resourceType in resourceBrokerConfig.pools) {
-	if (Array.isArray(pools[resourceType])) {
-		for (const poolId of pools[resourceType]) {
-			const resourcesForCurrentPool = resourcesInPool.get(poolId) || new Set();
-			resourcesForCurrentPool.add(resourceType);
+	for (const poolId of pools[resourceType]) {
+		const resourcesForCurrentPool = resourcesInPool.get(poolId) || new Set();
+		resourcesForCurrentPool.add(resourceType);
+		log.debug('ADD RES %s TO POOL %s', resourceType, poolId);
 
-			resourcesInPool.set(poolId, resourcesForCurrentPool);
-		}
+		resourcesInPool.set(poolId, resourcesForCurrentPool);
 	}
 }
 
