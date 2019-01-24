8dcd51f7b3ec283e786121cedf685846e5c49f4c
georgiy <mail@api.haus>
2019-01-24 15:51:39 +0300

    feat: improve stats output

--- src/bootstrap/worker/steps/stats.js
+++ src/bootstrap/worker/steps/stats.js
@@ -1,16 +1,18 @@
 import ms from 'ms';
 import config from 'config';
 import { collection } from 'stats/index';
+import { name as projectName } from 'project';
 import log from 'common/log';
 
-export default function workerStats({ workerId }) {
+export default function printStatsInterval() {
 	const statsInterval = ms(config.get('stats').interval);
-	setInterval(() => printStats(workerId), statsInterval);
+	setInterval(() => printStats(), statsInterval);
 }
 
-export function printStats(workerId) {
-	const object = collection.toJSON();
+export function printStats() {
+	const object = collection.toJSON()[projectName] || {};
 	for (let key in object) {
-		log.info('𝞢 (w%s) %s: %o', workerId, key, object[key]);
+		log.info('𝞢 %s: %o', key, object[key]);
 	}
+	log.info('┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉');
 }
