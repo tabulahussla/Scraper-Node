5986cb8a893761608ef21c80134449468172bab2
georgiy <mail@api.haus>
2019-02-07 16:29:50 +0300

    chore: deps

--- src/resources/pools.js
+++ src/resources/pools.js
@@ -1,16 +1,10 @@
 import config from 'config';
-import log from 'common/log';
 
 const resourceBrokerConfig = { ...config.get('resource-broker') };
 const { pools = {} } = resourceBrokerConfig;
 
 const resourcesInPool = new Map();
 
-/*
-"pools": {
-	"RESOURCE_TYPE": ["POOL_ID_1", "POOL_ID_2"],
-},
-*/
 for (const resourceType in resourceBrokerConfig.pools) {
 	for (const poolId of pools[resourceType]) {
 		const resourcesForCurrentPool = resourcesInPool.get(poolId) || new Set();
