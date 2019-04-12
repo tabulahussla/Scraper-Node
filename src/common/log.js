48d121262e18329c8de0fb5810bfb550fb90528b
Georgy Chirkov <mail@api.haus>
2019-04-12 14:25:46 +0300

    fix: log

--- src/common/log.js
+++ src/common/log.js
@@ -1,11 +1,17 @@
 import pino, { stdSerializers } from 'pino';
 import config from 'config';
 
+const isDev = process.env.NODE_ENV !== 'production';
+const prettyOptions = {
+	translateTime: 'SYS:yyyy-mm-dd HH:MM:ss',
+	ignore: 'name,pid,hostname',
+};
+
 const log = pino({
 	safe: true,
 	name: config.get('project-name'),
 	level: config.get('log-level'),
-	prettyPrint: process.env.NODE_ENV !== 'production',
+	prettyPrint: isDev && prettyOptions,
 	serializers: {
 		err: stdSerializers.err,
 	},
