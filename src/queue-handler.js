113f1a454b4d665e26100b18588dde60bf276564
Georgy Chirkov <mail@api.haus>
2019-04-16 19:06:43 +0300

    feat: retry scraping jobs

--- src/queue-handler.js
+++ src/queue-handler.js
@@ -3,6 +3,8 @@ import amqp from 'amqplib';
 import { amqpConnection } from 'database/amqp';
 import log from 'common/log';
 
+export const RetryHeader = '_retries';
+
 export default class QueueHandler {
 	/**
 	 * Creates an instance of QueueHandler.
@@ -11,12 +13,14 @@ export default class QueueHandler {
 	 * @param {number} options.concurrency
 	 * @param {string} options.queue
 	 * @param {(payload: any) => Promise<any>} options.worker
+	 * @param {number} [options.retries]
 	 * @memberof QueueHandler
 	 */
-	constructor({ concurrency, queue, worker }) {
+	constructor({ concurrency, queue, worker, retries = 10 }) {
 		this._queue = queue;
 		this._concurrency = concurrency;
 		this._worker = worker;
+		this._retries = retries;
 		/** @type {amqp.Channel} */
 		this._channel = null;
 	}
@@ -31,23 +35,47 @@ export default class QueueHandler {
 		await this._channel.assertQueue(this._queue);
 		await this._channel.consume(this._queue, msg => {
 			if (msg !== null) {
-				this._process(msg)
-					.then(() => this._channel.ack(msg))
-					.catch(err => {
-						log.error(err);
-						this._channel.reject(msg, true);
-					});
+				this.consumeMessage(msg);
 			}
 		});
 	}
 
 	/**
-	 * @param {amqp.ConsumeMessage | null} msg
+	 * @param {amqp.ConsumeMessage} msg
 	 * @memberof QueueHandler
 	 */
-	async _process(msg) {
-		// @ts-ignore
-		const contract = JSON.parse(msg.content);
-		await this._worker(contract);
+	async consumeMessage(msg) {
+		log.debug(
+			'consume message from "%s" queue: %o (headers: %o)',
+			this._queue,
+			msg.content.toString(),
+			msg.properties.headers,
+		);
+
+		let contract;
+		try {
+			// @ts-ignore
+			contract = JSON.parse(msg.content);
+		} catch (err) {
+			log.error('failed to parse message', { err }, msg.content);
+			this._channel.nack(msg, false, false);
+			return;
+		}
+		this._channel.ack(msg);
+		this._worker(contract).catch(err => {
+			log.error('failed to process contract', { err }, contract);
+			log.error(err);
+
+			// retry
+			let numTries = this._retries;
+			if (RetryHeader in msg.properties.headers) {
+				numTries = +msg.properties.headers[RetryHeader];
+			}
+			if (numTries > 0) {
+				numTries -= 1;
+				const headers = { ...msg.properties.headers, [RetryHeader]: numTries };
+				this._channel.sendToQueue(this._queue, msg.content, { headers });
+			}
+		});
 	}
 }
