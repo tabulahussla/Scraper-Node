14b668c875e2649b25274d9e7da95a51d7c482ad
Georgy Chirkov <mail@api.haus>
2019-04-16 19:05:38 +0300

    fix: setup channel for harvesting publishing

--- src/bootstrap/steps/database/amqp.js
+++ src/bootstrap/steps/database/amqp.js
@@ -1,10 +1,13 @@
 import config from 'config';
 import { connect } from 'database/amqp';
 import log from 'common/log';
+import { setupChannel } from 'harvest';
 
 export default async function connectAmqpConfigured() {
 	const amqpConfig = config.get('amqp');
 	await connect(amqpConfig);
 
 	log.debug('connected to amqp');
+
+	await setupChannel();
 }
